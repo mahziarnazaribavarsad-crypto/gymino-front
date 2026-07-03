4efe42d2f240a05538581979ff740cdfb1e9656c
mahziar <mahziarnazaribavarsad@gmail.com>
2026-07-04 02:44:42 +0800

    creating athleteHome footer

---
+++ assets/js/athleteHome.js
@@ -0,0 +1,17 @@
+const footerCol = document.querySelectorAll(".footer-col")
+
+footerCol.forEach(item => {
+
+    item.addEventListener('click' , () => {
+
+        footerCol.forEach(item => {
+            item.classList.remove('text-white')
+            item.classList.add('text-[#8E8E93]')
+        })
+
+        item.classList.remove('text-[#8E8E93]')
+        item.classList.add('text-white')
+
+    })
+
+})
