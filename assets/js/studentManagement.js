5171644da164c4433391cad7ccb53059be64427e
mahziar <mahziarnazaribavarsad@gmail.com>
2026-07-14 15:31:48 +0330

    create student management page

---
+++ assets/js/studentManagement.js
@@ -0,0 +1,19 @@
+// change color footer item after click
+
+const footerCol = document.querySelectorAll(".footer-col")
+
+footerCol.forEach(item => {
+
+    item.addEventListener('click' , () => {
+
+        footerCol.forEach(item => {
+            item.classList.remove('text-white')
+            item.classList.add('text-[#8E8E93]')
+        })
+
+        item.classList.remove('text-[#8E8E93]')
+        item.classList.add('text-white')
+
+    })
+
+})
